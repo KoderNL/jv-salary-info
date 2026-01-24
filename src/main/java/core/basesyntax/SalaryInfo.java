9744b2d1f13d4a24821e1822c2801b497fb2ccaf
Serhii Nikolaiev <haccent12@gmail.com>
2026-01-24 17:01:00 +0100

    changed to more clear naming of constant, made new consatant SEPARATOR, changed start values of int's variablas from constant NULL to 0

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,60 +4,60 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int DATE = 0;
-    private static final int NAME = 1;
-    private static final int HOURS = 2;
-    private static final int RATE = 3;
-    private static final int DAY = 1;
-    private static final int NULL = 0;
+    private static final int DATE_OF_WORK = 0;
+    private static final int EMPLOYEE_NAME = 1;
+    private static final int WORKING_HOURS_PER_DAY = 2;
+    private static final int SALARY_RATE = 3;
+    private static final int ONE_DAY = 1;
+    private static final String SEPARATOR = " - ";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
-        LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, FORMATTER).minusDays(DAY);
-        LocalDate dateToLocalDate = LocalDate.parse(dateTo, FORMATTER).plusDays(DAY);
+        LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, FORMATTER).minusDays(ONE_DAY);
+        LocalDate dateToLocalDate = LocalDate.parse(dateTo, FORMATTER).plusDays(ONE_DAY);
         int lastIndexOfDataLines = data.length - 1;
         int lastIndexOfNames = names.length - 1;
-        int indexOfName = NULL;
-        int salary = NULL;
-        int linesToWrite = NULL;
-        int wroteLines = NULL;
+        int indexOfName = 0;
+        int salary = 0;
+        int linesToWrite = 0;
+        int wroteLines = 0;
 
-        stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
+        stringBuilder.append("Report for period ").append(dateFrom).append(SEPARATOR).append(dateTo)
                 .append(System.lineSeparator());
 
-        for (int i = NULL; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             String[] currentString = data[i].split(" ");
-            LocalDate dateOfCurrentString = LocalDate.parse(currentString[DATE], FORMATTER);
+            LocalDate dateOfCurrentString = LocalDate.parse(currentString[DATE_OF_WORK], FORMATTER);
             if (dateOfCurrentString.isAfter(dateFromLocalDate)
                     && dateOfCurrentString.isBefore(dateToLocalDate)) {
-                if (indexOfName == NULL) {
+                if (indexOfName == 0) {
                     linesToWrite++;
                 }
-                if (currentString[NAME].equals(names[indexOfName])) {
-                    salary += Integer.parseInt(currentString[HOURS])
-                            * Integer.parseInt(currentString[RATE]);
+                if (currentString[EMPLOYEE_NAME].equals(names[indexOfName])) {
+                    salary += Integer.parseInt(currentString[WORKING_HOURS_PER_DAY])
+                            * Integer.parseInt(currentString[SALARY_RATE]);
                     wroteLines++;
                 }
             }
             if (i == lastIndexOfDataLines && linesToWrite > wroteLines) {
-                stringBuilder.append(names[indexOfName]).append(" - ").append(salary)
+                stringBuilder.append(names[indexOfName]).append(SEPARATOR).append(salary)
                         .append(System.lineSeparator());
-                salary = NULL;
-                i = NULL;
+                salary = 0;
+                i = 0;
                 indexOfName++;
             }
             if (i == lastIndexOfDataLines && linesToWrite == wroteLines
-                    && linesToWrite != NULL) {
-                stringBuilder.append(names[indexOfName]).append(" - ").append(salary);
+                    && linesToWrite != 0) {
+                stringBuilder.append(names[indexOfName]).append(SEPARATOR).append(salary);
             }
-            if (i == lastIndexOfDataLines && linesToWrite == NULL && indexOfName < names.length) {
-                stringBuilder.append(names[indexOfName]).append(" - ").append(salary);
+            if (i == lastIndexOfDataLines && linesToWrite == 0 && indexOfName < names.length) {
+                stringBuilder.append(names[indexOfName]).append(SEPARATOR).append(salary);
                 if (indexOfName != lastIndexOfNames) {
                     stringBuilder.append(System.lineSeparator());
                 }
                 indexOfName++;
-                i = NULL;
+                i = 0;
             }
         }
         return stringBuilder.toString();
