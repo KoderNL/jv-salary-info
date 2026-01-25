ca8c7d4503ef0ea048125abefef59be29f419597
Serhii Nikolaiev <haccent12@gmail.com>
2026-01-25 16:43:06 +0100

    change loops logics

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -8,56 +8,35 @@ public class SalaryInfo {
     private static final int EMPLOYEE_NAME = 1;
     private static final int WORKING_HOURS_PER_DAY = 2;
     private static final int SALARY_RATE = 3;
-    private static final int ONE_DAY = 1;
     private static final String SEPARATOR = " - ";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
-        LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, FORMATTER).minusDays(ONE_DAY);
-        LocalDate dateToLocalDate = LocalDate.parse(dateTo, FORMATTER).plusDays(ONE_DAY);
-        int lastIndexOfDataLines = data.length - 1;
+        LocalDate dateFromLocalDate = LocalDate.parse(dateFrom, FORMATTER);
+        LocalDate dateToLocalDate = LocalDate.parse(dateTo, FORMATTER);
+        int[] salaries = new int[names.length];
         int lastIndexOfNames = names.length - 1;
-        int indexOfName = 0;
-        int salary = 0;
-        int linesToWrite = 0;
-        int wroteLines = 0;
 
         stringBuilder.append("Report for period ").append(dateFrom).append(SEPARATOR).append(dateTo)
                 .append(System.lineSeparator());
 
-        for (int i = 0; i < data.length; i++) {
-            String[] currentString = data[i].split(" ");
-            LocalDate dateOfCurrentString = LocalDate.parse(currentString[DATE_OF_WORK], FORMATTER);
-            if (dateOfCurrentString.isAfter(dateFromLocalDate)
-                    && dateOfCurrentString.isBefore(dateToLocalDate)) {
-                if (indexOfName == 0) {
-                    linesToWrite++;
-                }
-                if (currentString[EMPLOYEE_NAME].equals(names[indexOfName])) {
-                    salary += Integer.parseInt(currentString[WORKING_HOURS_PER_DAY])
-                            * Integer.parseInt(currentString[SALARY_RATE]);
-                    wroteLines++;
+        for (int i = 0; i < names.length; i++) {
+            for (String dataLine : data) {
+                String[] currentString = dataLine.split(" ");
+                LocalDate dateForValidation = LocalDate
+                        .parse(currentString[DATE_OF_WORK], FORMATTER);
+                if (!dateForValidation.isBefore(dateFromLocalDate)
+                        && !dateForValidation.isAfter(dateToLocalDate)) {
+                    if (names[i].equals(currentString[EMPLOYEE_NAME])) {
+                        salaries[i] += Integer.parseInt(currentString[WORKING_HOURS_PER_DAY])
+                                * Integer.parseInt(currentString[SALARY_RATE]);
+                    }
                 }
             }
-            if (i == lastIndexOfDataLines && linesToWrite > wroteLines) {
-                stringBuilder.append(names[indexOfName]).append(SEPARATOR).append(salary)
-                        .append(System.lineSeparator());
-                salary = 0;
-                i = 0;
-                indexOfName++;
-            }
-            if (i == lastIndexOfDataLines && linesToWrite == wroteLines
-                    && linesToWrite != 0) {
-                stringBuilder.append(names[indexOfName]).append(SEPARATOR).append(salary);
-            }
-            if (i == lastIndexOfDataLines && linesToWrite == 0 && indexOfName < names.length) {
-                stringBuilder.append(names[indexOfName]).append(SEPARATOR).append(salary);
-                if (indexOfName != lastIndexOfNames) {
-                    stringBuilder.append(System.lineSeparator());
-                }
-                indexOfName++;
-                i = 0;
+            stringBuilder.append(names[i]).append(SEPARATOR).append(salaries[i]);
+            if (i != lastIndexOfNames) {
+                stringBuilder.append(System.lineSeparator());
             }
         }
         return stringBuilder.toString();
